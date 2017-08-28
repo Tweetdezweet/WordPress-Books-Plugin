153f6f95ad4b3aee651bbf1853dc62cfb3567b6e
koen <koen.gabriels@gmail.com>
2017-08-28 15:11:18 +0200

    Adds main plugin file with headers

---
+++ oxfam-books.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Oxfam Books Plugin
+Plugin URI:  https://koengabriels.be
+Description: WordPress plugin to support the sales and inventory management of secondhand books for Oxfam
+Version:     20170828
+Author:      Koen Gabriëls
+Author URI:  https://koengabriels.be
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: oxfambooks
+Domain Path: /languages
+*/
